3ddde6484a029029125150c06f3b134ff80ba091
enzofabriciosolis <fabriciosolis96@mail.com>
2016-03-16 10:33:41 -0300

    Saludo al mundo

--- Laboratorio2Ejercitacion1/Laboratorio2Ejercitacion1/Program.cs
+++ Laboratorio2Ejercitacion1/Laboratorio2Ejercitacion1/Program.cs
@@ -9,6 +9,8 @@ namespace Laboratorio2Ejercitacion1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("HolaMundo");
+            Console.ReadLine();
         }
     }
 }
